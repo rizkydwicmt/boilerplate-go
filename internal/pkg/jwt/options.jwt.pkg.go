3ef9de253708cb9a9043347bd76180da7198a628
rizkydwicmt <rizkydwi.cmt@gmail.com>
2025-02-13 13:18:16 +0700

    feat: internal/pkg/jwt - save to redis

--- internal/pkg/jwt/options.jwt.pkg.go
+++ internal/pkg/jwt/options.jwt.pkg.go
@@ -4,16 +4,23 @@ import (
 	"time"
 )
 
-// Define constants
 const (
 	DefaultTokenExpiredTime = 0
 	DefaultSigningMethod    = "HS256"
+)
+
+type SaveMethodJWTEnum string
+
+const (
+	REDIS SaveMethodJWTEnum = "REDIS"
+	JWT   SaveMethodJWTEnum = "JWT"
 )
 
 type Options struct {
 	TokenExpiredTime time.Duration
 	TokenSecretKey   string
 	SigningMethod    string
+	SaveMethod       SaveMethodJWTEnum
 }
 
 func DefaultOptions(secretKey string) *Options {
@@ -21,5 +28,6 @@ func DefaultOptions(secretKey string) *Options {
 		TokenExpiredTime: DefaultTokenExpiredTime,
 		TokenSecretKey:   secretKey,
 		SigningMethod:    DefaultSigningMethod,
+		SaveMethod:       JWT,
 	}
 }
